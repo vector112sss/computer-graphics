9741edfc1d61f5899ca9cced79d6bb0a6ef742fa
vector112sss <44300996+vector112sss@users.noreply.github.com>
2019-12-14 07:39:56 +0530

    Update and rename README.md to Ques6.cpp

--- Ques6.cpp
+++ Ques6.cpp
@@ -1,4 +1,4 @@
-//Creating a Triangle Fan
+//creating a triangle strip
 #include "GL/freeglut.h"
 #include "GL/gl.h"
 float _angle=0.0f;
@@ -7,17 +7,12 @@ glClearColor(0.3,0.4,0.0,0.0);
 glClear(GL_COLOR_BUFFER_BIT);
 glColor3f(1.0,0.0,1.0);
 glOrtho(-1.0,1.0,-1.0,1.0,-1.0,1.0);
-glBegin(GL_TRIANGLE_FAN);
-glVertex2f(0.0,0.0);
-glVertex2f(0.0,0.3);
-glVertex2f(0.2,0.3);
-glVertex2f(0.3,0.2);
-glVertex2f(0.35,0.05);
-glVertex2f(0.3,-0.1);
-glVertex2f(0.2,-0.2);
-glVertex2f(0.0,-0.25);
-glVertex2f(-0.2,-0.2);
-glVertex2f(-0.3,-0.1);
+glBegin(GL_TRIANGLE_STRIP);
+glVertex2f(-0.6,-0.4);
+glVertex2f(-0.6,0.2);
+glVertex2f(-0.3,-0.3);
+glVertex2f(-0.2,0.3);
+glVertex2f(0.0,-0.1);
 glEnd();
 glFlush();
 }
@@ -26,7 +21,7 @@ glutInit(&argc,argv);
 glutInitDisplayMode(GLUT_SINGLE);
 glutInitWindowSize(900,700);
 glutInitWindowPosition(100,100);
-glutCreateWindow("OpenGl - Creating a Triangle Fan");
+glutCreateWindow("OpenGl - Creating a LineStrip");
 glutDisplayFunc(drawTriangle);
 glutMainLoop();
 return 0;
